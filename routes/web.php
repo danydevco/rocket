ef89c9f35f2519c851bb7c9aa94052004feaf3db
Daniel Jose Ruiz Gutierrez <developer@danydev.co>
2023-12-18 11:34:00 -0500

    Nuevos ajustes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->middleware(['rocket.api.response'])->group(function () {
     Route::post(config('rocket.route.auth.login'), [AuthController::class, 'login']);
 
-    Route::post(config('rocket.route.auth.password.email'), [AuthController::class, 'emailPassword'])->middleware(['throttle:1,1']);
+    Route::post(config('rocket.route.auth.password.email'), [AuthController::class, 'emailPassword']);
     Route::post(config('rocket.route.auth.password.reset'), [AuthController::class, 'resetPassword']);
 
     Route::middleware(['auth:sanctum'])->group(function () {
