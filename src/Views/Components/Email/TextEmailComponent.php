b00c04984c832c44f3159c04cedda9f9ac4242b0
Daniel Jose Ruiz Gutierrez <developer@danydev.co>
2024-04-13 11:49:43 -0500

    add component text in template email

--- src/Views/Components/Email/TextEmailComponent.php
+++ src/Views/Components/Email/TextEmailComponent.php
@@ -7,22 +7,24 @@ use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class TextEmailComponent extends Component {
-    public array $styles;
+    public string $type = '';
+    public array  $styles;
 
     /**
-     * Create a new component instance.
+     * @param array  $styles
+     * @param string $type
      */
-    public function __construct(array $styles) {
-        $defaultStyles = [
-            'text-align' => 'left',
-            'padding' => '0',
-            'color' => '#718096',
-            'font-size' => '16px',
-            'font-family' => 'Arial, sans-serif',
-            'line-height' => '1.5',
-            'width' => '100%',
-        ];
-        $this->styles  = array_merge($defaultStyles, $styles);
+    public function __construct(array $styles, string $type = '') {
+        $this->type = $type;
+
+        $customStyles = match ($type) {
+            'title' => $this->getStyleTitle(),
+            'subtitle' => $this->getStyleSubtitle(),
+            'paragraph' => $this->getStyleParagraph(),
+            default => [],
+        };
+
+        $this->styles  = array_merge($customStyles, $styles);
     }
 
     /**
@@ -39,5 +41,37 @@ class TextEmailComponent extends Component {
             'styleString' => $styleString,
         ]);
     }
+
+    private function getStyleTitle(): array {
+        return [
+            'text-align' => 'center',
+            'font-size' => '18px',
+            'color' => '#022047',
+            'font-weight' => '600',
+            'margin-bottom' => '12px',
+        ];
+    }
+
+    private function getStyleSubtitle(): array {
+        return [
+            'text-align' => 'left',
+            'font-size' => '16px',
+            'color' => '#718096',
+            'font-weight' => '500',
+            'margin-bottom' => '12px',
+        ];
+    }
+
+    private function getStyleParagraph(): array {
+        return [
+            'text-align' => 'left',
+            'font-size' => '16px',
+            'color' => '#718096',
+            'font-weight' => '400',
+            'margin-bottom' => '12px',
+        ];
+    }
+
+
 
 }
