ca6ad04b0cfdc1fb738664991014f157f52f3cce
Daniel Jose Ruiz Gutierrez <developer@danydev.co>
2024-04-13 12:40:39 -0500

    add component text in template email

--- resources/views/mail/templates/header.blade.php
+++ resources/views/mail/templates/header.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ $url }}" style="display: inline-block;">
-    <img src="{{ config('rocket.app.logo') }}" style="height: auto; line-height: 100%; outline: none; text-decoration: none; display: block; width: 96px; border-style: none; border-width: 0;" width="96" alt="app">
+    <img src="{{ config('rocket.app.logo') }}" style="height: auto; line-height: 100%; outline: none; text-decoration: none; display: block; width: 250px; border-style: none; border-width: 0;" width="250" alt="app">
 </a>
 
